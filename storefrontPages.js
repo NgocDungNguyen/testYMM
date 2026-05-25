543130dc1c2ecb29d3f70ccaa1abacd3540db88b
JuneNguyen <156218862+NgocDungNguyen@users.noreply.github.com>
2026-05-25 13:47:27 +0700

    Add files via upload

---
+++ storefrontPages.js
@@ -0,0 +1,120 @@
+window.storefrontPagesData = [
+  {
+    "id": "hp",
+    "name": "Home Page",
+    "type": "system"
+  },
+  {
+    "id": "ac",
+    "name": "All Collections",
+    "type": "system"
+  },
+  {
+    "id": "coll-body",
+    "name": "Body Parts",
+    "type": "collection",
+    "target": "Body"
+  },
+  {
+    "id": "coll-braking",
+    "name": "Braking Parts",
+    "type": "collection",
+    "target": "Braking"
+  },
+  {
+    "id": "coll-cooling",
+    "name": "Cooling Parts",
+    "type": "collection",
+    "target": "Cooling"
+  },
+  {
+    "id": "coll-drivetrain",
+    "name": "Drivetrain Parts",
+    "type": "collection",
+    "target": "Drivetrain"
+  },
+  {
+    "id": "coll-electrical",
+    "name": "Electrical Parts",
+    "type": "collection",
+    "target": "Electrical"
+  },
+  {
+    "id": "coll-engine",
+    "name": "Engine Parts",
+    "type": "collection",
+    "target": "Engine"
+  },
+  {
+    "id": "coll-exhaust",
+    "name": "Exhaust Parts",
+    "type": "collection",
+    "target": "Exhaust"
+  },
+  {
+    "id": "coll-exterior",
+    "name": "Exterior Parts",
+    "type": "collection",
+    "target": "Exterior"
+  },
+  {
+    "id": "coll-filters",
+    "name": "Filters Parts",
+    "type": "collection",
+    "target": "Filters"
+  },
+  {
+    "id": "coll-fuel-system",
+    "name": "Fuel System Parts",
+    "type": "collection",
+    "target": "Fuel System"
+  },
+  {
+    "id": "coll-hvac",
+    "name": "HVAC Parts",
+    "type": "collection",
+    "target": "HVAC"
+  },
+  {
+    "id": "coll-interior",
+    "name": "Interior Parts",
+    "type": "collection",
+    "target": "Interior"
+  },
+  {
+    "id": "coll-lighting",
+    "name": "Lighting Parts",
+    "type": "collection",
+    "target": "Lighting"
+  },
+  {
+    "id": "coll-performance",
+    "name": "Performance Parts",
+    "type": "collection",
+    "target": "Performance"
+  },
+  {
+    "id": "coll-steering",
+    "name": "Steering Parts",
+    "type": "collection",
+    "target": "Steering"
+  },
+  {
+    "id": "coll-suspension",
+    "name": "Suspension Parts",
+    "type": "collection",
+    "target": "Suspension"
+  },
+  {
+    "id": "coll-transmission",
+    "name": "Transmission Parts",
+    "type": "collection",
+    "target": "Transmission"
+  },
+  {
+    "id": "coll-wheels-tires",
+    "name": "Wheels & Tires Parts",
+    "type": "collection",
+    "target": "Wheels & Tires"
+  }
+];
